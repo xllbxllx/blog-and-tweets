11e7bb40fed4abebda3e59ce1074df6698bdba9f
bryan cañas <wertty0831@gmail.com>
2020-07-25 10:37:25 -0500

    Add notes about listing last 3 entries per user

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -9,6 +9,15 @@ class GuestController extends Controller
 {
     public function index()
     {
+
+      // A GROUP_CONCAT with LIMIT is not supported in MySQL
+      // so we could add an additional column to the entries table,
+      // to keep track of the last 3 entries of each user.
+      // That boolean column would facilitate to query the last 3 entries for each one.
+
+      // An alternative solution could be to develop a cache strategy.
+      // All of these in order to avoid multiple queries or processing lots of data via PHP.
+
       $entries = Entry::with('user')
       ->orderByDesc('created_at')
       ->orderByDesc('id')
